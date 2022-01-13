1c4c574ec4371123eeef48540ec4d6b49c38ed75
snippetfinder <snippetfinder@protonmail.com>
2022-01-13 11:52:26 -0600

    JavaScript fixes

--- snippets/js/array/iterate/iterate.js
+++ snippets/js/array/iterate/iterate.js
@@ -1,5 +1,3 @@
 var array = [5, 4, 3, 2, 1];
-for (var index in array)  // ≡
-    console.log(index, ':', array[index]);  // 0 : 5  1 : 4  ...  4 : 1
-for (var item of array)   // ≡
-    console.log(item);                      // 5  4  ...  1
+for (var index in array) console.log(index, ':', array[index]);  // 0 : 5  1 : 4  ...  4 : 1  ≡
+for (var item of array) console.log(item);                       // 5  4  ...  1              ≡
--- snippets/js/array/join/join.js
+++ snippets/js/array/join/join.js
@@ -1,4 +1,4 @@
-var array = [1, 2, 3, 4, 5];
+var array = [1, 2.3, 'abc', -5, "123"];
 var separator = '-';
 var string = array.join(separator);  // ≡
-console.log(string);  // 1-2-3-4-5
+console.log(string);  // 1-2.3-abc--5-123
