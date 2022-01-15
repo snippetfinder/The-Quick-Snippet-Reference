fe6cbf03f07f81a956fcbf59e51ba02add983f4e
snippetfinder <snippetfinder@protonmail.com>
2022-01-14 21:50:21 -0600

    Fixed JavaScript

--- snippets/js/const/const.js
+++ snippets/js/const/const.js
@@ -1,9 +1,9 @@
 const number = 10;
 const decimal = 2.3;
-const string = "Hello there.";                     // ≡
+const string = "Hello there.";              // ≡
 const array = [2.3, 'Hello there.', [1, 2], {a: 1, b: 2}];
 const dictionary = {number: 2.3, "list": [1, 2], 'values': {a: 1, b: 2}};
-console.log(string);  // Hello there.                 ≡
+console.log(string);  // Hello there.          ≡
 
 try
 {
