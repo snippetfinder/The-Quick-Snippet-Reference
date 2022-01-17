75513aec19c7c2b5ef5c1cfd497f6e05b0f297d5
snippetfinder <snippetfinder@protonmail.com>
2022-01-17 11:57:52 -0600

    Fixed Python code

--- snippets/py/array/remove clear/remove.py
+++ snippets/py/array/remove clear/remove.py
@@ -9,5 +9,5 @@ array = [5, 4, 3, 2, 1]
 start = 1  # 0: first item, 1: second item...
 count = 3
 print(array)  # [ 5, 4, 3, 2, 1 ]
-del array[start:start + count]  # ≡
+del array[start:(start + count)]  # ≡
 print(array)  # [ 5, 1 ]
