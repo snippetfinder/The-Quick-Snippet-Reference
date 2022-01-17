8de8721798e0aff43b44630ced7a380413ed4a44
snippetfinder <snippetfinder@protonmail.com>
2022-01-17 10:32:52 -0600

    Fixed Rust code

--- snippets/rs/var/var.rs
+++ snippets/rs/var/var.rs
@@ -2,11 +2,11 @@ fn main()
 {
     let mut number = 10;
     let mut decimal = 2.3;
-    let mut string = "Hello there.";
+    let mut string = "Hello there.";     // ≡
     let mut array_numbers = vec![1, 2, 3, 4, 5];
     let mut array_decimals = vec![2.3, 3.14, 0.99];
     let mut array_strings = vec!["abc", "xyz", "123"];
     println!("{string}");  // Hello there.  ≡
-    string = "Goodbye!";
+    string = "Goodbye!";                 // ≡
     println!("{string}");  // Goodbye!      ≡
 }
