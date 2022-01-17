68ed10f6485d82ddcaf1dffb6e13a847ed983ed7
snippetfinder <snippetfinder@protonmail.com>
2022-01-17 10:33:57 -0600

    New encoding section

---
+++ snippets/js/encode/uri encode/uri.js
@@ -0,0 +1,15 @@
+var url = "https://example.com";
+var query = 'text="hello"&page=3';
+var parameter = 'black&white';
+console.log(encodeURI(url + '?' + query));                     // https://example.com?text=%22hello%22&page=3  ≡
+console.log(url + '?style=' + encodeURIComponent(parameter));  // https://example.com?style=black%26white      ≡
+/* ❯❯❯ GENERATOR:TABLE:JAVASCRIPT:CompareEscape()
+function CompareEscape()
+{
+    var table = [['character', 'encodeURI', 'encodeURIComponent', 'escape']];
+    var characters = ['*', '.', '_', '-', '~', "'", '!', '(', ')', '/', '+', '@', '?', '=', ':', '#', ';', ',', '$', '&', ' ', '%', '^', '[', ']', '{', '}', '<', '>', '"', '\\', '|', '`'];
+    for (var character of characters)
+        table.push([character, encodeURI(character), encodeURIComponent(character), escape(character)]);
+    return table;
+}
+GENERATOR ❮❮❮ */
