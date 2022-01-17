77bede0349bcb175e7a9d6b4aa28bbbd34ab5903
snippetfinder <snippetfinder@protonmail.com>
2022-01-17 11:58:06 -0600

    Fixed Rust code

--- snippets/rs/array/cut/cut.rs
+++ snippets/rs/array/cut/cut.rs
@@ -1,10 +1,10 @@
 fn main()
 {
     let mut array = vec![5, 4, 3, 2, 1];
-    let index = 1;  // 0: first item, 1: second item...
+    let start = 1;  // 0: first item, 1: second item...
     let count = 3;
     println!("{array:?}");      // [5, 4, 3, 2, 1]
-    let array_cut: Vec<_> = array.drain(index..(index + count)).collect();  // ≡
+    let array_cut: Vec<_> = array.drain(start..(start + count)).collect();  // ≡
     println!("{array:?}");      // [5, 1]
     println!("{array_cut:?}");  // [4, 3, 2]
 }
--- snippets/rs/array/remove clear/remove.rs
+++ snippets/rs/array/remove clear/remove.rs
@@ -8,16 +8,16 @@ fn main()
 
     // many items:
     let mut array = vec![5, 4, 3, 2, 1];
-    let index = 1;  // 0: first item, 1: second item...
+    let start = 1;  // 0: first item, 1: second item...
     let count = 3;
     println!("{array:?}");  // [5, 4, 3, 2, 1]
-    array.drain(index..(index + count));  // ≡
+    array.drain(start..(start + count));  // ≡
     println!("{array:?}");  // [5, 1]
 
     // many items at the end:
     let mut array = vec![5, 4, 3, 2, 1];
-    let index = 2;  // 0: first item, 1: second item...
+    let start = 2;  // 0: first item, 1: second item...
     println!("{array:?}");  // [5, 4, 3, 2, 1]
-    array.truncate(index);  // ≡
+    array.truncate(start);  // ≡
     println!("{array:?}");  // [5, 4]
 }
