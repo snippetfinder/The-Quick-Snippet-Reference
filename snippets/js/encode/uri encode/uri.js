d4e5f959a308bd8085290a1bc3d9ec61442cdd7e
snippetfinder <snippetfinder@protonmail.com>
2022-01-20 15:00:07 -0600

    JavaScript fixes

--- snippets/js/encode/uri encode/uri.js
+++ snippets/js/encode/uri encode/uri.js
@@ -3,10 +3,12 @@ var query = 'text="hello"&page=3';
 var parameter = 'black&white';
 console.log(encodeURI(url + '?' + query));                     // https://example.com?text=%22hello%22&page=3  ≡
 console.log(url + '?style=' + encodeURIComponent(parameter));  // https://example.com?style=black%26white      ≡
+
+// Comparison of encoding methods:  ≡
 /* ❯❯❯ GENERATOR:TABLE:JAVASCRIPT:Compare()
 function Compare()
 {
-    var table = [['character', 'encodeURI', 'encodeURIComponent', 'escape']];
+    var table = [['character', 'encodeURI', 'encodeURIComponent', 'escape (deprecated)']];
     var characters = ['*', '.', '_', '-', '~', "'", '!', '(', ')', '/', '+', '@', '?', '=', ':', '#', ';', ',', '$', '&', ' ', '%', '^', '[', ']', '{', '}', '<', '>', '"', '\\', '|', '`'];
     for (var character of characters)
         table.push([character, encodeURI(character), encodeURIComponent(character), escape(character)]);
